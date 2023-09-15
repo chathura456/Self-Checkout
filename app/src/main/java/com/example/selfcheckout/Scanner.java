c69eeedfb55e4a94b524104c4d36e46ac38e1dd2
chathura456 <chathuracde@gmail.com>
2023-09-15 15:22:08 +0530

    barcode scanner added

--- app/src/main/java/com/example/selfcheckout/Scanner.java
+++ app/src/main/java/com/example/selfcheckout/Scanner.java
@@ -3,11 +3,17 @@ package com.example.selfcheckout;
 
 import static android.app.PendingIntent.getActivity;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class Scanner extends AppCompatActivity {
 
@@ -20,7 +26,35 @@ public class Scanner extends AppCompatActivity {
         NavigationUtils.setBackNavigationClickListener(backBtn, this);
 
         Button detailsBtn = findViewById(R.id.detailsBtn);
-        NavigationUtils.setNavigationClickListener(detailsBtn, Scanner.this, ItemDetails.class);
+        //NavigationUtils.setNavigationClickListener(detailsBtn, Scanner.this, ItemDetails.class);
+
+        detailsBtn.setOnClickListener(v -> {
+            scanCode();
+        });
 
     }
+
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to Flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Scanner.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 }
